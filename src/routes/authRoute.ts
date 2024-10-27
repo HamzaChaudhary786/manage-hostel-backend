40fbb2d5aa8b5da51922468d9c660e894c37a402
HamzaChaudhary786 <hmzsattar99@gmail.com>
2024-10-27 07:40:42 -0700

    Inside authRoute file add multer storage function

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 import { createCurrentUser, getCurrentUser, updateCurrentUser } from '../controllers/authController';
 import { jwtCheck, jwtParse } from '../middlewear/auth';
-
+import multer from 'multer';
 
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, //5mb
+    },
+});
+
 router.post('/', jwtCheck, createCurrentUser);
 router.get('/', jwtCheck, jwtParse, getCurrentUser);
-router.put('/', jwtCheck, jwtParse, updateCurrentUser);
+router.put('/', upload.single("imageFile"), jwtCheck, jwtParse, updateCurrentUser);
 
 
 
